e12c194bd217e0a6164bcb39f5744063ec02cd54
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-28 15:32:26 -0400

    Add Comparable mixin to Node

--- lib/node.rb
+++ lib/node.rb
@@ -2,11 +2,19 @@
 
 # Defines a node for the tree
 class Node
+  include Comparable
+
   attr_accessor :data, :left, :right
 
   def initialize(data = nil, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
+  end
+
+  def <=>(other)
+    return nil if other.nil?
+
+    data <=> other.data
   end
 end
