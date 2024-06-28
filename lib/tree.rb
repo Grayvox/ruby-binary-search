a450cda39d161f75b55984f910c615bcb4753bfe
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-28 17:31:57 -0400

    Add Tree object
    * Adds the Tree class with @array for the tree's
    values and @root for the root node
    
    * Adds #build_tree and #balance_tree to generate
    the tree itself

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,1 +1,33 @@
 # frozen_string_literal: true
+
+require_relative 'node'
+
+# Defines the tree of nodes
+class Tree
+  attr_reader :root
+
+  def initialize(array)
+    @array = array.uniq.sort
+    @root = build_tree(@array)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    last = array.length - 1
+
+    balance_tree(array, 0, last)
+  end
+
+  def balance_tree(array, first, last)
+    return nil if first > last
+
+    middle = (first + last) / 2
+    root = Node.new(array[middle])
+
+    root.left = balance_tree(array, first, middle - 1)
+    root.right = balance_tree(array, middle + 1, last)
+
+    root
+  end
+end
