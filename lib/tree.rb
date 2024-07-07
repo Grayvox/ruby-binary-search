6abba1edd8e3fe8c23ea69954990ca4c51c3a336
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-07-07 14:02:28 -0400

    Add utility module and tests (so far)

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'node'
+require_relative 'utils/tree_utils'
 
 # Defines the tree of nodes
 class Tree
+  include TreeUtils
+
   attr_reader :root
 
   def initialize(array)
@@ -110,79 +113,5 @@ class Tree
     return @root = build_tree(nodes) if change_root
 
     build_tree(nodes)
-  end
-
-  private
-
-  def balance_tree(array, first, last)
-    return nil if first > last
-
-    middle = (first + last) / 2
-    root = Node.new(array[middle])
-
-    root.left = balance_tree(array, first, middle - 1)
-    root.right = balance_tree(array, middle + 1, last)
-
-    root
-  end
-
-  def internal_insert(value, node = @root)
-    return Node.new(value) if node.nil?
-
-    case value <=> node.data
-    when -1
-      node.left = internal_insert(value, node.left)
-    when 1
-      node.right = internal_insert(value, node.right)
-    when 0
-      node
-    end
-
-    node
-  end
-
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
-  def internal_delete(value, node = @root)
-    return node unless node
-
-    case value <=> node.data
-    when -1
-      node.left = internal_delete(value, node.left)
-    when 1
-      node.right = internal_delete(value, node.right)
-    when 0
-      return node.right if node.left.nil?
-      return node.left if node.right.nil?
-
-      node.data = min_value(node.right)
-      node.right = internal_delete(min_value(node.right), node.right)
-    end
-
-    node
-  end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
-
-  def queue_level_shift(queue)
-    first = queue.first
-
-    queue.push(first.left) if first.left
-    queue.push(first.right) if first.right
-    queue.shift
-
-    queue
-  end
-
-  def check_for_block(node, result, &block)
-    result << node.data if block_given? && block.call(node.data)
-    result << node.data unless block_given?
-    result
-  end
-
-  def min_value(node, min = node.data)
-    min_value(node.left, node.left.data) if node.left
-
-    min
   end
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,36 @@
 # frozen_string_literal: true
+
+require_relative 'lib/tree'
+
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# Test methods
+puts '--- #pretty_print ---'
+tree.pretty_print # returns a visual representation of the tree
+puts '--- #insert ---'
+tree.insert(11) # inserts 11 into the tree and rebalances it accordingly
+tree.pretty_print
+puts '--- #delete ---'
+tree.delete(11) # removes 11 from the tree and rebalances it accordingly
+tree.pretty_print
+puts '--- #find ---'
+p tree.find(4) # returns the node '4' that was found, with no left or right value
+puts '--- #level_order ---'
+p(tree.level_order { |element| element < 5 }) # returns all nodes less than 5 in level order
+puts '--- #level_order_iteration ---'
+p(tree.level_order_iteration { |element| element < 5 }) # returns all nodes less than 5 in level order
+puts '--- #preorder ---'
+p(tree.preorder { |element| element >= 5 }) # returns all nodes greater than or equal to five in preorder
+puts '--- #inorder ---'
+p(tree.inorder { |element| element != 1 }) # returns all nodes NOT equal to 1 in inorder
+puts '--- #postorder ---'
+p tree.postorder # returns all nodes in postorder
+puts '--- #height ---'
+
+puts '--- #depth ---'
+
+puts '--- #balanced? ---'
+p tree.balanced? # returns true
+puts '--- #rebalance ---'
+tree.rebalance # does nothing because it is already balanced
+tree.pretty_print # returns the tree
